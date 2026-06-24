32afff789e4174182fefc91a7b0c4c94aebe9838
olig76-design <olig76@gmail.com>
2026-06-24 23:31:14 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = "loftverkfaeri-v1";
+const assets = ["./", "./index.html", "./styles.css", "./app.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+    ))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
